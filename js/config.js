07041427339e892f1a64193576696570255426f2
Loopjazz <moyunusmo@gmail.com>
2025-05-07 17:33:16 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_nUKNTtWiE', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-west-2_RtcDmIZwu', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7tjkvehtepeqlgu55m7lq9bjt2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
